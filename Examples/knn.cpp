91484840066fe99af9b7ad7c0e475f857233e0ef
cneubauerUSGS <cneubauer@live.com>
2019-01-31 22:52:09 -0700

    It compiles now and uses manhattan distance calculation

--- Examples/knn.cpp
+++ Examples/knn.cpp
@@ -3,11 +3,16 @@
 #include <functional>
 #include <array>
 #include <iostream>
+#include "knn.h"
 
-int Predict1toMaxNeighbors(double *train_inputs_ptr, double *train_label_ptr,
+int Predict1toMaxNeighbors(
+  // inputs
+  double *train_inputs_ptr, double *train_label_ptr,
   int nrow, int ncol, int max_neighbors,
   double *test_input_ptr, // ncol
-  double *test_prediction_ptr) // max_neighbors {
+  // output
+  double *test_prediction_ptr) // max_neighbors
+  {
 
     // Eigen Library that we will be using:
     // VectorXd(n)
@@ -17,27 +22,25 @@ int Predict1toMaxNeighbors(double *train_inputs_ptr, double *train_label_ptr,
 
     Eigen::VectorXd distance_vec(nrow);
     Eigen::Map< Eigen::MatrixXd > train_inputs_mat(train_inputs_ptr, nrow, ncol);
-    Eigen::Map< Eigen::MatrixXd > train_inputs_vec(test_inputs_ptr, ncol);
-    Eigen::VectorXd diff_vec(ncol);
+    Eigen::Map< Eigen::VectorXd > test_input_vec(test_input_ptr, ncol);
     Eigen::VectorXi sorted_index_vec(nrow);
-    
+
     int row = 0;
     int neighbors = 0;
     double total_labels = 0;
 
     // Step 1: Compute Distances
     for(int i=0; i<nrow; i++) {
-      diff_vec = train_inputs_mat.row(i).transpose() - test_input_vec;
-      distance_vec(i) = diff_vec.norm();
+      distance_vec(i) = (train_inputs_mat.row(i).transpose()-test_input_vec).array().abs().sum();
       sorted_index_vec(i) = i;
-
     }
+
     // Print before sorting
     // std::cout << sorted_index_vec << std::endl;
 
     // Step 2: Sort Indicies based on Indicies
     std::sort(sorted_index_vec.data(), // Gets first element in sorted_index_vec
-              sorted_index_vec.data() + sorted_index_vec.size()) // Gets last element in sorted_index_vec
+              sorted_index_vec.data() + sorted_index_vec.size()), // Gets last element in sorted_index_vec
 
               // Lambda Function to sort based on distances
               [&distance_vec](int left, int right) {
@@ -46,7 +49,7 @@ int Predict1toMaxNeighbors(double *train_inputs_ptr, double *train_label_ptr,
     // Print after sorting
     // std::cout << sorted_index_vec << std::endl;
 
-    // Step 3: Compute Neighbors (Apply Equation)
+    // // Step 3: Compute Neighbors (Apply Equation)
     for(int k=0; k<max_neighbors; k++) {
 
       row = sorted_index_vec(k);
@@ -59,3 +62,42 @@ int Predict1toMaxNeighbors(double *train_inputs_ptr, double *train_label_ptr,
     }
     return 0;
   }
+
+
+int main()
+{
+  int nrow = 3;
+  int ncol = 2;
+  int max_neighbors = 3;
+  int n = nrow * ncol;
+
+  double *train_inputs_ptr = new double[n];
+  for (int i = 0; i < n; i+= 1)
+  {
+    train_inputs_ptr[i] = double(i + 1.0);
+  }
+  double *train_label_ptr = new double[nrow];
+  for (int i = 0; i < nrow; i+= 1)
+  {
+    train_label_ptr[i] = double(i + 1);
+  }
+  double *test_input_ptr = new double[ncol];
+
+  for (int i = 0; i < ncol; i+= 1) {
+    test_input_ptr[i] = double(i + 1.5);
+  }
+
+
+  double *test_prediction_ptr = new double[nrow];
+
+  Predict1toMaxNeighbors( train_inputs_ptr, train_label_ptr,
+                          nrow, ncol, max_neighbors,
+                          test_input_ptr, test_prediction_ptr);
+
+  for(int i = 0; i < nrow; i++)
+  {
+    std::cout << "neighbor # prediction: " << test_prediction_ptr[i] << '\n';
+  }
+
+  return 0;
+}
---
+++ Examples/knn.h
@@ -0,0 +1,4 @@
+int Predict1toMaxNeighbors(  double *train_inputs_ptr, double *train_label_ptr,
+                             int nrow, int ncol, int max_neighbors,
+                             double *test_input_ptr,
+                             double *test_prediction_ptr);
