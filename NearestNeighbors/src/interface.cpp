f910fd81b463250b7fc4ccb8227989c93d81bf9a
caneubauer <coleneubauer@gmail.com>
2019-02-15 19:14:12 -0700

    fixed matrix knn

--- NearestNeighbors/src/interface.cpp
+++ NearestNeighbors/src/interface.cpp
@@ -27,28 +27,13 @@ void NN1toKmaxPredict(double *train_inputs_ptr, double *train_label_ptr,
   }
 }
 
-// Code required to register the interface function with R.
-R_CMethodDef cMethods[] = {
-  {"NN1toKmaxPredict", (DL_FUNC) &NN1toKmaxPredict, 7},
-  {"NN1toKmaxMatrixPredict", (DL_FUNC) &NN1toKmaxPredict, 7},
-
-  {NULL, NULL, 0}
-};
-
-extern "C" {
-  void R_init_NearestNeighbors(DllInfo *info) {
-    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
-    R_useDynamicSymbols(info, FALSE);
-  }
-}
-
 void NN1toKmaxMatrixPredict(double *train_inputs_ptr, double *train_label_ptr,
-                      int *n_observations, int *n_features, int *max_neighbors,
+                      int *n_observations, int *n_features, int *max_neighbors, int* n_test,
                       double *test_input_ptr,
                       double *test_prediction_ptr) {
 
   int status = NN1toKmaxMatPredict_C(train_inputs_ptr, train_label_ptr,
-                                  *n_observations, *n_features, *max_neighbors,
+                                  *n_observations, *n_features, *max_neighbors, *n_test,
                                   test_input_ptr,
                                   test_prediction_ptr);
 
@@ -63,5 +48,20 @@ void NN1toKmaxMatrixPredict(double *train_inputs_ptr, double *train_label_ptr,
   }
   if(status == ERROR_NO_TEST_DATA){
     error("no test data");
+  }
+}
+
+// Code required to register the interface function with R.
+R_CMethodDef cMethods[] = {
+  {"NN1toKmaxPredict", (DL_FUNC) &NN1toKmaxPredict, 7},
+  {"NN1toKmaxMatrixPredict", (DL_FUNC) &NN1toKmaxMatrixPredict, 8},
+
+  {NULL, NULL, 0}
+};
+
+extern "C" {
+  void R_init_NearestNeighbors(DllInfo *info) {
+    R_registerRoutines(info, cMethods, NULL, NULL, NULL);
+    R_useDynamicSymbols(info, FALSE);
   }
 }
