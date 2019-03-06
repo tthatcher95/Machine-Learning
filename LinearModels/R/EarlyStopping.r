ff5ecc81eff64a2aea02819d6bdda4e7abfad3f4
Tyler Thatcher <tthatcher84@gmail.com>
2019-03-06 12:37:32 -0700

    Updated EarlyStopping with proper gradient descent

--- LinearModels/R/EarlyStopping.r
+++ LinearModels/R/EarlyStopping.r
@@ -1,19 +1,38 @@
-LMSquareLossIterations <- function(x.mat, y.vec, max.iterations, step.size) {
-  W.v = list()
-  append(W.v, 0)
-  W.t.0 = 0
-  for(x in seq(0, step.size, max.iterations)) {
-    print(x)
-    W.t.1 = W.t.0 - step.size * 2 * (W.t.0)
-    append(W.v, W.t.1)
-    W.t.0 = W.t.1
+LMSquareLossIterations <- function(x.unsc.mat, y.vec, max.iterations, step.size) {
+  x.mat <- scale(x.unsc.mat)
+  W.v <- numeric(ncol(x.mat))
+  mean.mat <- attr(x.mat, "scaled:center")
+  sd.mat <- attr(x.mat, "scaled:scaled")
+  
+  # R Scaling/Filtering
+  ## Filters out variablity == 0
+  # is.zero <- attr(X.sc, "scaled:scale") == 0
+  # x.mat <- X.sc[, !is.zero]
+  # keep <- attr(X.sc, "scaled:scale") != 0
+  
+  ## Make X-Matrix 
+  # x.mat <- X.sc[, keep]
+  
+  ## Unscale Matrix
+  # W.original.scale[, keep] <- W.mat / attr(X.sc, "scaled:scale)
+  
+  W.mat = matrix(NA, ncol(x.mat + 1), max.iterations)
+  W.mat[, 1] <- W.v
+  
+  for(iteration in 2:max.iterations) {
+    gradient <- 2 * t(x.mat) %*% ((x.mat %*% W.v)  - y.vec) 
+    W.v <- W.v - step.size * gradient
+    W.mat[, iteration] <- W.v
   }
-  return(W.v)
+  return(W.mat)
 }
 
-Wt = LMSquareLossIterations(c(1, 2 ,3 ,4, 5), c(5, 5, 3, 2, 1), 5, 0.5)
-print(Wt)
 
 LMLogisticLossIterations <- function(x.mat, y.vec, max.iterations, step.size) {
 
 }
+
+data = ElemStatLearn::ozone
+data[, -1]
+sum = LMSquareLossIterations(data[, -1], data[, 1], 3, 0.35)
+print(sum)
