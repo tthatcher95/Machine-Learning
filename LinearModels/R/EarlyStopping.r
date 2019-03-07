255b812ce1fb853f909892538168a2eefecdf5c3
Tyler Thatcher <tthatcher84@gmail.com>
2019-03-07 16:22:00 -0700

    Finished SquareCV/EarlyStopping

--- LinearModels/R/EarlyStopping.r
+++ LinearModels/R/EarlyStopping.r
@@ -1,7 +1,5 @@
-UNscale <- function(scaled.matrix) {
-  scaled.matrix * attr(scaled.matrix,'scaled:scale') + attr(scaled.matrix, 'scaled:center')
-  return(scaled.matrix)
-}
+data = ElemStatLearn::ozone
+
 orig.mat = data[, -1]
 x.mat <- scale(orig.mat)
 
@@ -9,9 +7,10 @@ x.unsc.mat <- data[,-1]
 y.vec <-data[,1]
 max.iterations=10
 step.size=0.35
+
 LMSquareLossIterations <- function(x.unsc.mat, y.vec, max.iterations, step.size) {
+  
   x.mat <- scale(x.unsc.mat)
-  #orig.scale = t(apply(x.mat, 1, function(r) r*attr(x.mat,'scaled:scale') + attr(x.mat, 'scaled:center')))
   W.v <- numeric(ncol(x.mat))
   mean.mat <- attr(x.mat, "scaled:center")
   sd.mat <- attr(x.mat, "scaled:scaled")
@@ -33,21 +32,45 @@ LMSquareLossIterations <- function(x.unsc.mat, y.vec, max.iterations, step.size)
   
   for(iteration in 2:max.iterations) {
     gradient <-  t(x.mat) %*% (x.mat %*% W.v  - y.vec)/nrow(x.mat)
-    print(gradient)
     W.v <- W.v - step.size * gradient
     W.mat[, iteration] <- W.v
   }
   W.orig.mat = W.mat/attr(x.mat, "scaled:scale")
   intercept = -t(W.mat/ attr(x.mat, "scaled:scale")) %*% attr(x.mat, "scaled:center")
-  all.equal(x.mat %*% W.mat, cbind(1, as.matrix(x.unsc.mat)) %*% W.out)
+  # print(all.equal(x.mat %*% W.mat, cbind(1, as.matrix(x.unsc.mat)) %*% W.out))
   W.out = rbind(t(intercept), W.orig.mat)
-  W.out
+  return(W.out)
 }
 
-LMSquareLossEarlyStoppingCV<- function(X.mat, y.vec, fold.vec, max.iterations=100, step.size=0.01) {
-  # Make CV select this
-  selected.steps=7
+LMSquareLossEarlyStoppingCV<- function(X.mat, y.vec, fold.vec=NULL, max.iterations=100, step.size=0.05) {
+  
+  if(is.null(fold.vec)) {
+    fold.vec <- sample(rep(1:4, l=nrow(X.mat)))
+  }
+  
+  W.valid <- matrix(NA, 4, max.iterations)
+  
+  for(fold in 1:4) {
+    fold_data <- which(fold.vec == fold)
+    
+    X.train <- X.mat[-fold_data ,]
+    X.valid <- X.mat[fold_data ,]
+
+    Y.train <- y.vec[-fold_data]
+    Y.valid <- y.vec[fold_data]
+    
+    W.train <- LMSquareLossIterations(X.train, Y.train, max.iterations, step.size)
+    W.valid[fold , ] = colMeans((cbind(1, as.matrix(X.valid)) %*% W.train - Y.valid)^2)
+  }
+  
+  mean.valid.vec = colMeans(W.valid)
+  
+  min.valid.mean = which(mean.valid.vec == min(mean.valid.vec), arr.ind = TRUE)
+
+  selected.steps = min.valid.mean
+  
   w.vec = LMSquareLossIterations(X.mat, y.vec, selected.steps, step.size)[, selected.steps]
+  
   list(
     predict=function(testX.mat){
       cbind(1, as.matrix(testX.mat)) %*% w.vec
@@ -60,6 +83,3 @@ fit <- LMSquareLossEarlyStoppingCV(x.unsc.mat, y.vec)
 LMLogisticLossIterations <- function(x.mat, y.vec, max.iterations, step.size) {
 
 }
-
-data = ElemStatLearn::ozone
-LMSquareLossIterations(data[, -1], data[, 1], 3, 0.35)
